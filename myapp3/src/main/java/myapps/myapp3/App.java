5eafb2289aec474a00b04a57d4ddbe0a889baff0
David <David@jts.demo.com>
2015-10-29 05:47:47 +0800

    Signed-off-by: David <David@jts.demo.com>

--- myapp3/src/main/java/myapps/myapp3/App.java
+++ myapp3/src/main/java/myapps/myapp3/App.java
@@ -1,4 +1,4 @@
-package com.demo.app;
+package myapps.myapp3;
 
 /**
  * Hello world!
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World -- 3 !" );
+        System.out.println( "Hello World -- 1 !" );
     }
 }
